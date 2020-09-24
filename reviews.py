b5820502ae0ab6ab8473112ba818bffc51d1ca4e
ming1208 <weng022768@gmail.com>
2020-09-24 21:43:37 +0800

    Add calculate avg.lenth

--- reviews.py
+++ reviews.py
@@ -1,4 +1,5 @@
 # 增加count功能,掌握文件讀取狀態
+# 增加計算留言的平均長度
 data = []
 count = 0
 with open('reviews.txt','r') as f:
@@ -10,8 +11,14 @@ with open('reviews.txt','r') as f:
 
 print(len(data))
 print('檔案內容總共有', len(data), '筆留言')
-print('第一筆留言內容如下: ')
-print(data[0])
-print('--------------------------------------------------------------------')
-print('第二筆留言內容如下: ')
-print(data[1])
+# print('第一筆留言內容如下: ')
+# print(data[0])
+# print('--------------------------------------------------------------------')
+# print('第二筆留言內容如下: ')
+# print(data[1])
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	print(sum_len)
+print('--------------------------------------------------------------------')	
+print('留言的平均長度: ',sum_len/len(data))
