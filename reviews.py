8235a53b5ebe0637b596d04975221ab73b7a8a00
ming1208 <weng022768@gmail.com>
2020-10-01 18:40:13 +0800

    add other function

--- reviews.py
+++ reviews.py
@@ -20,5 +20,25 @@ sum_len = 0
 for d in data:
 	sum_len += len(d)
 	print(sum_len)
-print('--------------------------------------------------------------------')	
+
 print('留言的平均長度: ',sum_len/len(data))
+print('--------------------------------------------------------------------')   
+
+#增加留言長度<100的篩選功能
+new =[]
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('總共有', len(new), '筆留言長度小於100')
+print('第一筆留言內容如下: ')
+print(new[0])
+print('--------------------------------------------------------------------')
+print('第二筆留言內容如下: ')
+print(new[1])
+
+#統計留言內容提到good的筆數
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('總共有', len(good), '筆留言提到good')
